bd4e6e9d73374adb5c079caab36350bc1b31a27c
VietPQ <20522153@gm.uit.edu.vn>
2026-08-12 13:29:57 +0700

    ci: them job lint check code quality

--- src/main/java/com/vietpq/baseline/SpringBootJvmBaselineApplication.java
+++ src/main/java/com/vietpq/baseline/SpringBootJvmBaselineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootJvmBaselineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootJvmBaselineApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJvmBaselineApplication.class, args);
+    }
 
 }
