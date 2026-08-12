46b3d8d989b1e899cea294e6ca1259ff5a360624
VietPQ <20522153@gm.uit.edu.vn>
2026-08-12 14:02:59 +0700

    ci: delete checkStyle + ruleSet pmd | Use Spotless + spotbug | Use toml version

--- src/main/java/com/vietpq/baseline/SpringBootJvmBaselineApplication.java
+++ src/main/java/com/vietpq/baseline/SpringBootJvmBaselineApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootJvmBaselineApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootJvmBaselineApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootJvmBaselineApplication.class, args);
+  }
 }
